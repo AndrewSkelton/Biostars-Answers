d91c1f8342b48b8bdde4b54214f98208b0072cf8
Andrew Skelton <andrew.j.skelton73@gmail.com>
2017-02-15 15:08:28 +0000

    Handle reading in a file

--- p236827.R
+++ p236827.R
@@ -2,11 +2,20 @@ source("https://bioconductor.org/biocLite.R")
 biocLite("IlluminaHumanMethylation450kanno.ilmn12.hg19")
 
 library(dplyr)
+library(readr)
 library(IlluminaHumanMethylation450kanno.ilmn12.hg19)
 data("IlluminaHumanMethylation450kanno.ilmn12.hg19")
 
+#Manually Enter CpGs
 query_cpgs <- c("cg23786580")
-anno       <- IlluminaHumanMethylation450kanno.ilmn12.hg19 %>% 
-              getAnnotation %>% 
+
+#Read in CpGs from File
+#Extract the "CpGs" Column
+query_cpgs <- read_csv("my_cpgs.csv") %>% 
               as.data.frame %>% 
-              dplyr::slice(match(query_cpgs, Name))
+              .[["CpGs"]]
+
+anno       <- IlluminaHumanMethylation450kanno.ilmn12.hg19 %>% 
+  getAnnotation %>% 
+  as.data.frame %>% 
+  dplyr::slice(match(query_cpgs, Name))
